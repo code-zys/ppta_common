75fad29a9d42995577792a660d0fbe4c920ec93f
cedric tanda <tandacedric@gmail.com>
2024-09-12 14:57:31 +0100

    Fix module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[],
     description='Prepa Compta common module',
