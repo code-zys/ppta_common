5f2ede7897b79fd0bddbfb23d86bfbae9ac7b32b
cedric tanda <tandacedric@gmail.com>
2024-09-12 14:53:55 +0100

    update structure

---
+++ ppta-common/ppta_common.py
@@ -0,0 +1,6 @@
+def ma_fonction():
+    print("Hello depuis mon module !")
+
+def test_cedric(name:str):
+    print("Ceci est un test HTTC ")
+    print(name)
--- ppta_common.py
+++
@@ -1,2 +0,0 @@
-def ma_fonction():
-    print("Hello depuis mon module !")
