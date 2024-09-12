2bad6e211cba0d00d91eaaad6015bf934def4d4d
cedric tanda <tandacedric@gmail.com>
2024-09-12 15:08:27 +0100

    Rename all module

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ppta-common',
-    version='0.2',
+    name='ppta_common',
+    version='0.1',
     packages=find_packages(),
     install_requires=[],
     description='Prepa Compta common module',
     author='Peter',
-    url='https://github.com/code-zys/ppta-common',
+    url='https://github.com/code-zys/ppta_common',
 )
