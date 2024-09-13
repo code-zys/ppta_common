02af3a2f6777890165baf87acee7d41ec14d13e1
Peter Tsegwi <tsegwipeter@gmail.com>
2024-09-13 15:00:07 +0100

    added collect rule response

---
+++ ppta_common/dtos/response/collect_rule_response.py
@@ -0,0 +1,18 @@
+from pydantic import Field, BaseModel
+
+
+from typing import Any, Dict, Optional
+
+class CollectRuleResponse(BaseModel):
+    id: str
+    title: Optional[str]
+    subject: Optional[str]
+    collected_from: Optional[str]
+    has_attachment: bool
+    before: str
+    after: str
+    contain: Optional[str]
+    connection: Dict[str, Any] = Field(..., description="Détails de la connexion")
+    invoice_id: str
+    last_connection_date: Optional[int]
+    last_connection_status: Optional[str]
