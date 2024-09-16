abe1860608be66e82ac0b16738a9e3795595a086
Peter Tsegwi <tsegwipeter@gmail.com>
2024-09-16 14:41:47 +0100

    added title to run delivery rule

--- ppta_common/models/run_delivery_rule.py
+++ ppta_common/models/run_delivery_rule.py
@@ -9,6 +9,7 @@ class RunDeliveryRule(EmbeddedDocument):
     mode = EnumField(DeliveryRuleModeEnum, default=DeliveryRuleModeEnum.API)
     receiver = StringField()
     receiver_mail = StringField()
+    title = StringField()
 
     def __str__(self):
-        return f"RunDeliveryRule<mode = {self.mode}, receiver = {self.receiver}, receiver_mail = {self.receiver_mail}>"
+        return f"RunDeliveryRule<title = {self.title}, mode = {self.mode}, receiver = {self.receiver}, receiver_mail = {self.receiver_mail}>"
