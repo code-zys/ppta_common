f6313178abb88290955c08ece81544ce182fdd0e
Peter Tsegwi <tsegwipeter@gmail.com>
2024-09-23 11:55:03 +0100

    added invoice class permission dto

---
+++ ppta_common/dtos/request/update_invoice_class_visibility_dto.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing import Any, Dict, Optional
+from .address_dto import AddressDto
+from .timezone_dto import TimezoneDto
+from ...utils.enums import FrequencyEnum, EnumRole
+
+class UpdateInvoiceClassPermission(BaseModel):
+    invoice_class_id: str
+    year: str
+    month: str
+    frequency: FrequencyEnum
+    roles: list[EnumRole]
+
+    def to_dict(self) -> Dict[str, Any]:
+            """Return the dictionary representation of the model using alias.
+
+            This has the following differences from calling pydantic's
+            `self.model_dump(by_alias=True)`:
+
+            * `None` is only added to the output dict for nullable fields that
+            were set at model initialization. Other fields with value `None`
+            are ignored.
+            """
+            _dict = self.model_dump(
+                by_alias=True,
+                exclude={
+                },
+                exclude_none=True,
+            )
+            return _dict
