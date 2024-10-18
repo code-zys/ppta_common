883b06a77f8d1c162f96fd68e748da1a25a8e046
Peter Tsegwi <tsegwipeter@gmail.com>
2024-10-18 13:15:22 +0100

    update notification model

--- ppta_common/models/notification.py
+++ ppta_common/models/notification.py
@@ -4,7 +4,17 @@ from .base_document import BaseDocument
 from .user import User
 from .company import Company
 from ..utils.enums import EnumRole
+from enum import Enum
 
+
+class NotificationType(str, Enum):
+    COMPANY = 'COMPANY'
+    MEMBER = 'MEMBER'
+    GENERAL = 'GENERAL'
+
+class NotificationStatus(str, Enum):
+    READ = 'READ'
+    UNREAD = 'UNREAD'
 
 class Notification(BaseDocument):
     """
@@ -12,13 +22,13 @@ class Notification(BaseDocument):
     """
     room = StringField(required=True)
     message = StringField(required=True)
-    notification_type = StringField(default="general")
-    object = DictField()
-    status = EnumField(choices=["unread", "read"], default="unread")
+    notification_type = StringField(choices=[e.value for e in NotificationType], required=True)
+    data = DictField()
+    status = StringField(choices=[e.value for e in NotificationStatus], required=True, default=NotificationStatus.UNREAD.value)
+    from_user = ReferenceField(User, required=True)
     to_user = ReferenceField(User, required=False)
-    to_company = ReferenceField(Company)
+    to_company = ReferenceField(Company, required=False)
     recipient_roles = ListField(StringField(choices=[e.value for e in EnumRole], required=True), default=[])
-    created_at = IntField(default=None)
 
     def __str__(self):
         return f"Notification:{self.room}, {self.message}"
