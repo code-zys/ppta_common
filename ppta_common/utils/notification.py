8ab3a318f1aecc92fb8f8e68798bd0b031ad2a8f
Peter Tsegwi <tsegwipeter@gmail.com>
2024-10-18 16:44:02 +0100

    added notification logs

--- ppta_common/utils/notification.py
+++ ppta_common/utils/notification.py
@@ -7,7 +7,10 @@ class NotificationService:
     @staticmethod
     def send(url: str, payload: Notification):
         result = NotificationService.create(payload)
-        requests.post(url, data=json.dumps(result.to_dict()))
+        notification_data:str = json.dumps(result.to_dict())
+        print('Notificaiton data:: ', notification_data)
+        post_result = requests.post(url, data=notification_data)
+        print('Post result:: ', post_result)
         return result
 
 
