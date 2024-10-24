604c8d5a561245484906a1606b41ee2c9232b7b6
Peter Tsegwi <tsegwipeter@gmail.com>
2024-10-24 23:37:06 +0100

    added constraint on pydantic version to 2.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.1.66',
+    version='0.1.67',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
@@ -10,7 +10,7 @@ setup(
         'pytz',
         'boto3',
         'blinker',
-        'pydantic',
+        'pydantic==2.2.2',
         'pydantic-settings',
         'python-jose',
         'imap_tools',
