4d6e7bd811b4e082bf6aa7e7340e00d936fd9007
Peter Tsegwi <tsegwipeter@gmail.com>
2024-12-04 12:47:13 +0100

    added powens attribute in company

--- ppta_common/models/company.py
+++ ppta_common/models/company.py
@@ -33,3 +33,4 @@ class Company(BaseDocument):
     monthCountMember = IntField(required=True, default = 0)
     yearCountMember = IntField(required=True, default = 0)
     logo = StringField(required=False)
+    powens_access_token = StringField(required=False)
