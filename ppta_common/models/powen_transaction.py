3bfdfd5168edecea29c513409594a49ae40dbb6d
Peter Tsegwi <tsegwipeter@gmail.com>
2024-12-06 15:46:23 +0100

    added powen transactions model

---
+++ ppta_common/models/powen_transaction.py
@@ -0,0 +1,41 @@
+from .base_document import BaseDocument
+from mongoengine import (
+    IntField,
+    StringField,
+    FloatField,
+    BooleanField,
+    DictField
+)
+
+class Transaction(BaseDocument):
+    account_id = StringField(required=True, default="")
+    webid = StringField(required=True, default="")
+    application_date = StringField(required=True, default="")
+    date = StringField(required=True, default="")
+    datetime = StringField(required=True, default="")
+    value = FloatField(required=True, default=0.0)
+    state = StringField(required=True, default="")
+    scraped_date = StringField(required=True, default="")
+    coming = BooleanField(required=True, default=False) 
+    active = BooleanField(required=True, default=True) 
+    cluster_id = StringField(required=True, default=None)
+    comment = StringField(required=True, default=None)
+    last_update = StringField(required=True, default=None)
+    deleted = BooleanField(required=True, default=False)
+    original_value = FloatField(required=True, default=None)
+    original_gross_value = StringField(required=True, default=None)
+    original_currency = StringField(required=True, default=None)
+    commission = FloatField(required=True, default=None)
+    commission_currency = StringField(required=True, default=None)
+    card = StringField(required=True, default=None)
+    type = StringField(required=True, default=None)
+    new = BooleanField(required=True, default=False)
+    formatted_value = StringField(required=True, default=None)
+    documents_count = IntField(required=True, default=0)
+    informations = DictField()
+    country = StringField(required=True, default=None)
+    countryparty = StringField(required=True, default=None)
+    meta = {
+        'collection': 'powen_transactions',
+        "strict": False
+    }
