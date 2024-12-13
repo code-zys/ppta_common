26c422d7344a17e31d4e5fce483db3c3ac791454
Peter Tsegwi <tsegwipeter@gmail.com>
2024-12-13 08:52:50 +0100

    added company reference to bank account

--- ppta_common/models/bank_account.py
+++ ppta_common/models/bank_account.py
@@ -5,6 +5,8 @@ from mongoengine import (
     ReferenceField,
     DateField
 )
+
+from ppta_common.models.company import Company
 from .bank_connection import BankConnection
 from .base_document import BaseDocument
 from ..enums.bank_provider_enum import BankProviderEnum
@@ -19,7 +21,8 @@ class BankAccount(BaseDocument):
 
 
     bank_connection = ReferenceField(BankConnection)
-
+    company = ReferenceField(Company)
+    
     from_sync_date =  DateField(required=False)
     to_sync_date = DateField(required = False)
     last_sync_date = DateField(required=False)
