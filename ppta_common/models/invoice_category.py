439e3544f7715694a0a4ffe2d3f34608fb684c00
Peter Tsegwi <tsegwipeter@gmail.com>
2024-12-16 13:41:32 +0100

    added invoice category model

---
+++ ppta_common/models/invoice_category.py
@@ -0,0 +1,18 @@
+from mongoengine import Document, StringField, BooleanField, ReferenceField
+from .company import Company
+from .base_document import BaseDocument
+
+class InvoiceCategory(BaseDocument):
+    name = StringField(required=True, max_length=255)
+    code = StringField(required=True, unique=True, max_length=50)
+    description = StringField(max_length=500)
+    is_system = BooleanField(default=False)
+    company = ReferenceField(Company, required=True)
+
+    meta = {
+        'collection': 'invoice_categories',
+        'indexes': [
+            'code',
+            'company',
+        ]
+    }
