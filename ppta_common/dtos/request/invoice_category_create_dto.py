5b98c9ba11569874aa4cecb1364d9fc86212edb8
Peter Tsegwi <tsegwipeter@gmail.com>
2024-12-18 07:45:07 +0100

    renamed to invoice category create

--- ppta_common/dtos/request/invoice_category_create_dto.py
+++ ppta_common/dtos/request/invoice_category_create_dto.py
@@ -2,8 +2,7 @@ from typing import List, Optional
 from pydantic import BaseModel, validator
 from ...enums.role_enum import EnumRole
 
-class InvoiceCategoryDto(BaseModel):
+class InvoiceCategoryCreateDto(BaseModel):
     name: str
     code: str
     description: str
-    is_system: bool
