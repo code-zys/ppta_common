689a3d35d73065379764bd8d1fb9b90def84a6cf
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-06 06:51:53 +0100

    added bridge user uuid

--- ppta_common/models/company.py
+++ ppta_common/models/company.py
@@ -35,4 +35,4 @@ class Company(BaseDocument):
     logo = StringField(required=False)
     powens_access_token = StringField(required=False)
     powen_user_id = IntField(required=False)
-    bridge_user_id = StringField(required=False)
+    bridge_user_uuid = StringField(required=False)
