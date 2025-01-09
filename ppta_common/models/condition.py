068f6b0e66f82fb3ea399ff6c2603a2ef0ae6f5a
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-09 19:30:10 +0100

    update penality attribute

--- ppta_common/models/condition.py
+++ ppta_common/models/condition.py
@@ -2,5 +2,5 @@ from mongoengine import StringField, EmbeddedDocument
 
 class Condition(EmbeddedDocument):
     discount = StringField(default="", required=True)
-    panality = StringField(default="", required=True)
+    penality = StringField(default="", required=True)
     indeminity = StringField(default="", required=True)
