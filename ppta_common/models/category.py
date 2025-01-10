30e1696bf3d5154fde9ea6c402e8d5b769b5d5b8
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-10 13:51:40 +0100

    update category model

--- ppta_common/models/category.py
+++ ppta_common/models/category.py
@@ -4,17 +4,15 @@ from .base_document import BaseDocument
 
 class Category(BaseDocument):
     name = StringField(required=True)
-    number = StringField(required=False)
     code = StringField(required=True, unique=True)
     description = StringField(max_length=500)
     is_system = BooleanField(default=False)
     company = ReferenceField(Company, required=False)
-    active = BooleanField(required=True, default=True)
 
     meta = {
         'collection': 'categories',
         'indexes': [
-            'code',
+            'name',
             'company',
         ]
     }
