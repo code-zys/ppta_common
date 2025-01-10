f4198ee3954cff7b6b17f9460ee784c04a2eb11a
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-10 08:15:39 +0100

    remove constraints

--- ppta_common/models/category.py
+++ ppta_common/models/category.py
@@ -3,9 +3,9 @@ from .company import Company
 from .base_document import BaseDocument
 
 class Category(BaseDocument):
-    name = StringField(required=True, max_length=255)
+    name = StringField(required=True)
     number = StringField(required=False)
-    code = StringField(required=True, unique=True, max_length=50)
+    code = StringField(required=True, unique=True)
     description = StringField(max_length=500)
     is_system = BooleanField(default=False)
     company = ReferenceField(Company, required=False)
