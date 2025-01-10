fcbe07cb02f01c073f1a20edf7d2f34cadc7aae2
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-10 07:57:48 +0100

    added new fields in category

--- ppta_common/models/category.py
+++ ppta_common/models/category.py
@@ -4,10 +4,12 @@ from .base_document import BaseDocument
 
 class Category(BaseDocument):
     name = StringField(required=True, max_length=255)
+    number = StringField(required=False)
     code = StringField(required=True, unique=True, max_length=50)
     description = StringField(max_length=500)
     is_system = BooleanField(default=False)
     company = ReferenceField(Company, required=False)
+    active = BooleanField(required=True, default=True)
 
     meta = {
         'collection': 'categories',
