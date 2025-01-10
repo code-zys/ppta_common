5c393d27ae21a16cbb8a72c72c7875ff81d5e79c
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-10 13:59:10 +0100

    added parent in company

--- ppta_common/models/category.py
+++ ppta_common/models/category.py
@@ -4,15 +4,15 @@ from .base_document import BaseDocument
 
 class Category(BaseDocument):
     name = StringField(required=True)
-    code = StringField(required=True, unique=True)
+    code = StringField(required=True)
     description = StringField(max_length=500)
     is_system = BooleanField(default=False)
     company = ReferenceField(Company, required=False)
+    parent = ReferenceField('self', required = False)
 
     meta = {
         'collection': 'categories',
         'indexes': [
-            'name',
-            'company',
+            {'fields': ['code', 'name', 'company'], 'unique': True}
         ]
     }
