8cb28ad70f95cede704052b7c801562b5cdd6126
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-17 16:30:40 +0100

    update mission model

--- ppta_common/models/mission.py
+++ ppta_common/models/mission.py
@@ -1,4 +1,4 @@
-from mongoengine import StringField, FloatField, IntField, BooleanField, ListField, EmbeddedDocumentField, EnumField, DateField, TupleField, ReferenceField
+from mongoengine import StringField, FloatField, IntField, BooleanField, ListField, EmbeddedDocumentField, EnumField, DateField, ReferenceField
 from ppta_common.models.company import Company
 from .base_document import BaseDocument
 from ..enums.contract_type_enum import ContractType
@@ -13,8 +13,8 @@ class Mission(BaseDocument):
     job_title = StringField(required=True, description="Title of the job")
     reference = StringField(required=True, description="Reference identifier for the mission")
     contract_type = EnumField(ContractType, required=True, description="Type of contract for the mission")
-    daily_rate = TupleField(FloatField(min_value=0), description="Daily rate range for freelance contracts as a tuple (min, max)")
-    annual_gross_salary = TupleField(IntField(min_value=1), IntField(min_value=1), description="Annual gross salary range as a tuple of minimum and maximum")
+    daily_rate = ListField(FloatField(min_value=0), max_length=2, description="Daily rate range for freelance contracts as a tuple (min, max)")
+    annual_gross_salary = ListField(FloatField(min_value=1), max_length=2, description="Annual gross salary range as a tuple of minimum and maximum")
     is_remuneration_profile_based = BooleanField(required=True, description="Indicates if remuneration is based on the profile")
     experience = EnumField(ExperienceLevel, required=True, description="Experience level required for the mission")
     workplace = EmbeddedDocumentField(Workplace, required=True, description="Details about the workplace")
