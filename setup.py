07af8bdac08d739ac6567d0225d98df711ea22d1
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-27 11:13:17 +0100

    updated mission status enum

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.3.46',
+    version='0.3.47',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
