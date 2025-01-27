2c08e0be3dbacac6bea0c694e4d412b5cdc97429
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-27 10:58:36 +0100

    added mission status to mission update dto

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.3.45',
+    version='0.3.46',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
