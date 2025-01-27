992b5e8b6658dddd8e5a2cc6b0d85bd32277898c
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-27 08:45:51 +0100

    updated member dto

--- ppta_common/dtos/request/member_dto.py
+++ ppta_common/dtos/request/member_dto.py
@@ -1,19 +1,20 @@
 from typing import Any, Dict
 from pydantic import BaseModel
-
 from .professional_info_dto import ProfessionalInfoDto
 from ...utils.enums import EnumUserType
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class MemberDto(BaseModel):
-    professional_info: ProfessionalInfoDto
-    user_type:EnumUserType  
-
+    professional_info:Optional[ProfessionalInfoDto]= Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
+    user_type:Optional[EnumUserType]= Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
+    principal_skills:  Optional[dict] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
+    general_skills:  Optional[dict] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
+    job:Optional[dict] =Field(None, description="The main daily activities")
     def to_dict(self) -> Dict[str, Any]:
         """Return the dictionary representation of the model using alias.
-
         This has the following differences from calling pydantic's
         `self.model_dump(by_alias=True)`:
-
         * `None` is only added to the output dict for nullable fields that
         were set at model initialization. Other fields with value `None`
         are ignored.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.3.42',
+    version='0.3.43',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
