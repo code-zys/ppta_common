716b5baa5d74e7305bf31a44465c12acce777b62
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-30 12:04:55 +0100

    updated requirements of boto

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.3.67',
+    version='0.3.68',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
         'tzlocal',
         'pytz',
-        'boto3',
+        # 'boto3',
+        'boto3-stubs[s3,sqs,textract,kms]',
         'blinker',
         'pydantic',
         'pydantic-settings',
