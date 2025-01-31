4708a9abc415e3fce8ab73381f58dc74bffd3bae
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-31 13:03:37 +0100

    update enum respones

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.3.72',
+    version='0.3.73',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
