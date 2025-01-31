a8963b9c06eb3998a09f486186eb157ebd192ddf
Peter Tsegwi <tsegwipeter@gmail.com>
2025-01-31 11:05:19 +0100

    update setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.3.70',
+    version='0.3.71',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
