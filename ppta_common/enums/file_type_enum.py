5e4ab1623cefcb3c0df1356a2ddc650f015716f4
Peter Tsegwi <tsegwipeter@gmail.com>
2025-02-07 18:06:02 +0100

    added file type enum

---
+++ ppta_common/enums/file_type_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class EnumFileType(Enum):
+    PHOTO = "PHOTO"
+    LOGO = "LOGO"
+    CV = "CV"
+    OTHER="OTHER"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.4.7',
+    version='0.4.8',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
