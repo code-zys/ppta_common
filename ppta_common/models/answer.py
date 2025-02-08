8803fbe44ccbd5794a575674a45cd8ec7542cec5
cedric tanda <tandacedric@gmail.com>
2025-02-08 13:25:37 +0100

    Update answer models

--- ppta_common/models/answer.py
+++ ppta_common/models/answer.py
@@ -2,9 +2,9 @@ from mongoengine import EmbeddedDocument, StringField, FloatField, DictField, Da
 from ..enums.answer_status_enum import AnswerStatus
 
 class Answer(EmbeddedDocument):
-    answer = StringField(required=True)
+    answer = StringField(required=False)
     ia_matching = FloatField(required=False)
     question = DictField(required=True)
     start_time = DateTimeField(required=False)
-    ended_time = DateTimeField(required=False)
-    status = StringField(required=True,choices=[e for e in AnswerStatus])
+    end_time = DateTimeField(required=False)
+    status = StringField(required=True,choices=[e.value for e in AnswerStatus])
