253bb835dfe01126fd37b2bac568437427d10dda
Peter Tsegwi <tsegwipeter@gmail.com>
2025-02-10 11:31:08 +0100

    updated member models

--- ppta_common/dtos/request/member_dto.py
+++ ppta_common/dtos/request/member_dto.py
@@ -4,14 +4,21 @@ from .professional_info_dto import ProfessionalInfoDto
 from ...utils.enums import EnumUserType
 from pydantic import BaseModel, Field
 from typing import Optional
+
 class MemberDto(BaseModel):
-    professional_info:Optional[ProfessionalInfoDto]= Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
-    user_type:Optional[EnumUserType]= Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
-    key_skills:  Optional[dict] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
-    general_skills:  Optional[dict] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
-    know_how:  Optional[dict] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
-    job: Optional[dict] =Field(None, description="The main daily activities")
-    cv: Optional[str] =Field(None, description="The s3 key of the cv of the member")
+    professional_info: Optional[ProfessionalInfoDto] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
+    user_type: Optional[EnumUserType] = Field(..., description="List of expected skills, e.g., AWS, Python, Pandas")
+    key_skills: dict = Field(..., description="List of expected skills, e.g., AWS, Python, Pandas")
+    general_skills: Optional[dict] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
+    know_how: Optional[dict] = Field(None, description="List of expected skills, e.g., AWS, Python, Pandas")
+    job: Optional[dict] = Field(None, description="The main daily activities")
+    cv: Optional[str] = Field(None, description="The s3 key of the cv of the member")
+    other_cvs: list[str] = Field(None, description="The other cv(s) of the member")
+    photo: Optional[str] = Field(None, description="The s3 key of the picture of the member")
+    departments: list[str] = Field(..., description="The list of departments were the member can work")
+    min_average_daily_rate: float = Field(..., description="Min dailly rate ")
+    max_average_daily_rate: float = Field(..., description="Min dailly rate ")
+    availability_date: int = Field( ..., description="The availability date of the member")
 
     def to_dict(self) -> Dict[str, Any]:
         """Return the dictionary representation of the model using alias.
@@ -23,8 +30,7 @@ class MemberDto(BaseModel):
         """
         _dict = self.model_dump(
             by_alias=True,
-            exclude={
-            },
+            exclude={},
             exclude_none=True,
         )
         return _dict
--- ppta_common/models/member.py
+++ ppta_common/models/member.py
@@ -1,4 +1,4 @@
-from mongoengine import StringField, EmbeddedDocumentField, BooleanField, ReferenceField, IntField, DictField, URLField, ListField
+from mongoengine import StringField, EmbeddedDocumentField, BooleanField, ReferenceField, IntField, DictField, FloatField, ListField
 from .base_document import BaseDocument
 from .company import Company
 from .professional_info import ProfessionalInfo
@@ -20,8 +20,14 @@ class Member(BaseDocument):
     is_consultant = BooleanField(default=False)
     is_commercial = BooleanField(default=False)
     key_skills = DictField(EmbeddedDocumentField(Skill))
-    general_skills=  DictField(EmbeddedDocumentField(Skill))
+    general_skills=  DictField(EmbeddedDocumentField(Skill), required=False, description="The général skills of the member")
     know_how = DictField(EmbeddedDocumentField(KnowHow), required=False, description="List of soft skills, e.g., communication, confidence")
     job= EmbeddedDocumentField(Job, default=None)
     cv = StringField(required=False)
     other_cvs = ListField(StringField(), required=False, default = [])
+
+    photo = StringField(required=False, description="The S3 key of the picture of the member")
+    departments = ListField(StringField(), required=True,description="The list of departments were the member can work" )
+    min_average_daily_rate = FloatField(required=True)
+    max_average_daily_rate = FloatField(required=True)
+    availability_date = IntField(required=True, description="The availability date of the member")
