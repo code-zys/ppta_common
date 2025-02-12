543bd32592a7dafd61a14956f150a30353efeae9
Peter Tsegwi <tsegwipeter@gmail.com>
2025-02-12 17:26:48 +0100

    update mission model

--- ppta_common/models/mission.py
+++ ppta_common/models/mission.py
@@ -1,10 +1,10 @@
-from mongoengine import Document, StringField, DateTimeField, ReferenceField, EnumField
+from mongoengine import StringField, DateTimeField, ReferenceField, EnumField
 from .company import Company
 from .member import Member
 from .application import Application
 from ..enums.mission_status_enum import MissionStatus
-
-class Mission(Document):
+from .base_document import BaseDocument
+class Mission(BaseDocument):
     title = StringField(required=True)
     description = StringField()
     start_date = DateTimeField(required=True)
@@ -13,7 +13,7 @@ class Mission(Document):
     consultant = ReferenceField(Member, required=False)
     commercial = ReferenceField(Member)
     final_client = ReferenceField(Company, required=False)
-    
+
     status = EnumField(MissionStatus, required=True, description="Status of the mission", default=MissionStatus.PENDING)
     application = ReferenceField(Application,required=True)
     meta = {'collection': 'missions'}
