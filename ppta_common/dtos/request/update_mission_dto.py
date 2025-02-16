bd63c7b7a505bbf2888829c1bf435236d5cd24bf
Peter Tsegwi <tsegwipeter@gmail.com>
2025-02-16 16:56:12 +0100

    added mission status in mission update dto

--- ppta_common/dtos/request/update_mission_dto.py
+++ ppta_common/dtos/request/update_mission_dto.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
+from ...enums.mission_status_enum import MissionStatus
 
 class MissionUpdateDto(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
     start_date: Optional[datetime] = None
     end_date: Optional[datetime] = None
-    application_id: Optional[str] = None
+    # application_id: Optional[str] = None
     consultant_id: Optional[str] = None
+    status: Optional[MissionStatus] = None
