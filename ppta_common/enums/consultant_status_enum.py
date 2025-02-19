39df1e693c5f41ade0776a3cd5e494c422fd8803
Peter Tsegwi <tsegwipeter@gmail.com>
2025-02-19 20:02:52 +0100

    updated consultant status

--- ppta_common/enums/consultant_status_enum.py
+++ ppta_common/enums/consultant_status_enum.py
@@ -1,7 +1,6 @@
 from enum import Enum
-
-
 class EnumConsultantStatus(str, Enum):
-    ACCEPTED = 'ACCEPTED'
-    REFUSED = 'REFUSED'
-    PENDING = 'PENDING'
+    ACCEPTED = "ACCEPTED"
+    REFUSED = "REFUSED"
+    PENDING = "PENDING"
+    CANCELLED = "CANCELLED"
