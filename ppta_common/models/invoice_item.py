862dddd8069dd77a638e09f7de09c9fc00d04168
Peter Tsegwi <tsegwipeter@gmail.com>
2025-03-03 15:21:24 +0100

    removed default value on invoice item

--- ppta_common/models/invoice_item.py
+++ ppta_common/models/invoice_item.py
@@ -7,7 +7,7 @@ class InvoiceItem(EmbeddedDocument):
     description = StringField(required=True, default="")
     label = StringField(required=True, default="")
     unit_price_ht = FloatField(required=True, default=0.0)
-    tax_rate = FloatField(required=False, default=0.0)
+    tax_rate = FloatField(required=False)
     discountValue = FloatField(required=False, default=0.0)
     discountType = StringField(choices=[e.value for e in EnumDiscountType], required=False)
     unit = StringField(choices=[e.value for e in EnumMesureUnit], required=False)
