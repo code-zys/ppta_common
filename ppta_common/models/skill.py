1a6a3d965f29f3fa3d232dd2ee5c5e0d5c3aa69e
arnoldrx <tagnearnold2080@gmail.com>
2025-03-03 11:16:51 +0100

    fix

--- ppta_common/models/skill.py
+++ ppta_common/models/skill.py
@@ -1,8 +1,8 @@
-from mongoengine import EmbeddedDocument, StringField, IntField
+from mongoengine import EmbeddedDocument, StringField, IntField, FloatField
 
 class Skill(EmbeddedDocument):
     code = StringField(required=True)
     name = StringField(required=True)
-    percent = IntField(required=False)
+    percent = FloatField(required=False)
     link_to_video = StringField(required=False)
     description = StringField(required=False)
