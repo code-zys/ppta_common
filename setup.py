a7c14740655cbc6ba2f1f6aba7ef712b47f57a96
arnoldrx <tagnearnold2080@gmail.com>
2025-03-19 13:15:32 +0100

    add new enum

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.4.72',
+    version='0.4.73',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
