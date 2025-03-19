0178e35368899b3e668b124cadfbafbd92baf3bf
arnoldrx <tagnearnold2080@gmail.com>
2025-03-19 16:27:10 +0100

    add new attribute

--- ppta_common/models/timesheet.py
+++ ppta_common/models/timesheet.py
@@ -19,23 +19,19 @@ class Timesheet(BaseDocument):
     # project = StringField(default=None) #TODO: title of mission by default, can be updated
     # team = StringField(default=None)
     type = StringField(required=True,choices=[e.value for e in TimesheetType])
-
     published_by_company = ReferenceField(Company, required=True)
     published_for_company = ReferenceField(Company, required=True)
     supervisor = ReferenceField(Member)
-
     consultant_observation = StringField(default=None)
     supervisor_observation = StringField(default=None)
     company_observation = StringField(default=None)
-
     consultant_approved_at = IntField(default=None)
     supervisor_approved_at = IntField(default=None)
     company_approved_at = IntField(default=None)
-
+    total_worked_days = IntField(default=None)
     # consultant_approval_status = StringField(default=None,choices=[e.value for e in TimesheetApprovalStatus])
     # supervisor_approval_status = StringField(default=None,choices=[e.value for e in TimesheetApprovalStatus])
     # company_approval_status = StringField(default=None,choices=[e.value for e in TimesheetApprovalStatus])
-
     timetrack = ListField(EmbeddedDocumentField(TimeTrack),default=[])
     status = StringField(required=True, default=TimesheetApprovalStatus.DRAFT.value,choices=[e.value for e in TimesheetApprovalStatus])
     level = StringField(required=True,default=TimeSheetLevel.CONSULTANT.value,choices=[e.value for e in TimeSheetLevel])
