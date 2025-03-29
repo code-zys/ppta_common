ee27c52ff854aac4d3d7fd00f22fa17cb07f4630
arnoldrx <tagnearnold2080@gmail.com>
2025-03-29 17:00:32 +0100

    fix

--- ppta_common/models/google_meet.py
+++ ppta_common/models/google_meet.py
@@ -1,14 +1,14 @@
 import datetime
-from mongoengine import EmbeddedDocument, StringField, DateTimeField, IntField
+from mongoengine import EmbeddedDocument, StringField, DateTimeField, IntField, ListField
 
 class GoogleMeet(EmbeddedDocument):
     name = StringField(required=True)
     description = StringField(required=True)
     link = StringField(required=True)
     candidate_email = StringField(required=True)
-    recruter_email = StringField(required=False)
     interview_date = DateTimeField(required=True)
-    start_time: datetime
-    end_time: datetime
+    start_time = DateTimeField(required=True)
+    end_time = DateTimeField(required=True)
     notification_before = IntField(required=False)
+    additional_participants_email = ListField(StringField(), required=False)
     
