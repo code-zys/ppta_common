09aee732d29bbfbeb6128cacf6c0d260c867edf5
arnoldrx <tagnearnold2080@gmail.com>
2025-04-01 15:12:47 +0100

    fix: rollback

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.4.99',
+    version='0.5.0',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
