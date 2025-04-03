d3705bc1537134ab392fdac5840d689a63475b19
arnoldrx <tagnearnold2080@gmail.com>
2025-04-03 16:30:01 +0100

    fix

--- ppta_common/models/time_range.py
+++ ppta_common/models/time_range.py
@@ -1,12 +1,12 @@
-from mongoengine import EmbeddedDocument, StringField, BooleanField
+from mongoengine import EmbeddedDocument, StringField, BooleanField, DateField
 from ..enums.time_range_type_enum import EnumTimeRangeType
 
 class TimeRange(EmbeddedDocument):
-    date = StringField(required=True)
-    start_time = StringField(required=True)
-    end_time = StringField(required=True)
+    date = DateField(required=True)
+    start_time = DateField(required=True)
+    end_time = DateField(required=True)
     timezone = StringField(required=True)
     is_recursive = BooleanField(default=False)
-    end_date = StringField(required=False, null=True)
+    end_date = DateField(required=False, null=True)
     type = StringField(choices=[e.value for e in EnumTimeRangeType], required=True)
     coach_id = StringField(required=True)
