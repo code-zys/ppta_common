0584f4d9e0dc5c91d0bf4dae01790fb00209472f
arnoldrx <tagnearnold2080@gmail.com>
2025-04-14 18:11:11 +0100

    fix

--- ppta_common/enums/available_period_status_enum.py
+++ ppta_common/enums/available_period_status_enum.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-class AvailablePeriodStatus(Enum):
+class EnumAvailablePeriodStatus(str, Enum):
     ON_GOING = "ON_GOING"
     OCCUPIED = "OCCUPIED"
     NOT_OCCUPIED = "NOT_OCCUPIED"
