d33112409eb947633f2657d49a39d37cad2ad6d3
arnoldrx <tagnearnold2080@gmail.com>
2025-04-16 10:49:25 +0100

    fix

--- ppta_common/enums/financial_line_type_enum.py
+++ ppta_common/enums/financial_line_type_enum.py
@@ -1,5 +1,5 @@
 from enum import Enum
 
 class EnumFinancialLineType(str, Enum):
-    COACH = "COACH"
-    APP_FEE = "APP_FEE"
+    COACHING = "COACHING"
+    CANCELLED = "CANCELLED"
--- ppta_common/models/financial_line.py
+++ ppta_common/models/financial_line.py
@@ -1,18 +1,18 @@
-from mongoengine import StringField,IntField, ReferenceField, FloatField, BooleanField
-
+from mongoengine import StringField,IntField, ReferenceField, FloatField
 from .base_document import BaseDocument
 from ppta_common.models.session import Session
 from ppta_common.models.coaching import Coaching
 from ppta_common.models.coach import Coach
 from ppta_common.enums.financial_line_type_enum import EnumFinancialLineType
 from ppta_common.enums.financial_line_status_enum import EnumFinancialLineStatus
+from ppta_common.enums.destination_enum import EnumDestinationStatus
 
 class FinancialLine(BaseDocument):
     session = ReferenceField(Session, required=True)
     date = IntField(required=True)
-    transaction_id = StringField(required=True)
+    transaction_id = StringField(required=False)
     client = StringField(required=False)
-    amount = FloatField(required=True)
+    amount = FloatField(required=False)
     start_date  = IntField(required=True)
     end_date  = IntField(required=True)
     coaching = ReferenceField(Coaching, required=True)
@@ -21,4 +21,4 @@ class FinancialLine(BaseDocument):
     paid_at = IntField(default=False)
     status = StringField(choices=[e.value for e in EnumFinancialLineStatus], required=True)
     cancelled_at = IntField(required=False)
-    destination = StringField(choices=[e.value for e in EnumFinancialLineType], required=False)
+    destination = StringField(choices=[e.value for e in EnumDestinationStatus], required=False)
