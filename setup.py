1d56a80f9d12a933fe37f04e0d3f3432b71562c3
arnoldrx <tagnearnold2080@gmail.com>
2025-04-25 14:47:21 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.5.73',
+    version='0.5.74',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
