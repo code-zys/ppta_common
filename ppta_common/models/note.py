8bd6d37107852b08c75cd0c5e65773396d43b932
arnoldrx <tagnearnold2080@gmail.com>
2025-04-25 10:00:11 +0100

    fix

--- ppta_common/models/note.py
+++ ppta_common/models/note.py
@@ -1,9 +1,9 @@
 from mongoengine import StringField, ReferenceField, IntField
 from .base_document import BaseDocument
-from .coaching import Coaching
+from .session import Session
 class Note(BaseDocument):
     value = IntField(required=True)
     comment = StringField(required=False)
     client = StringField(required=True)
     coach_id = StringField(required=True)
-    coaching = ReferenceField(Coaching, required=True)
+    session = ReferenceField(Session, required=True)
