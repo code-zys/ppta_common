6ebd3c7ee245e3b38963595a12fc4c0ca2b3c8cb
cedric tanda <tandacedric@gmail.com>
2025-04-25 12:40:23 +0100

    Ajout des reasons sur les notes

---
+++ ppta_common/enums/session_note_reason.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class SessionNoteReason(Enum):
+    ABSENCE = "ABSENCE"
+    LANGUAGE_DEFAULT = "LANGUAGE_DEFAULT"
+    OTHER = "OTHER"
--- ppta_common/models/note.py
+++ ppta_common/models/note.py
@@ -1,9 +1,13 @@
 from mongoengine import StringField, ReferenceField, IntField
 from .base_document import BaseDocument
 from .session import Session
+from ..enums.session_note_reason import SessionNoteReason
+
 class Note(BaseDocument):
     value = IntField(required=True)
     comment = StringField(required=False)
+    reason  = StringField(choices=[e.value for e in SessionNoteReason], required=False, default=None)
     client = StringField(required=True)
     coach_id = StringField(required=True)
+    coaching_id = StringField(required=True)
     session = ReferenceField(Session, required=True)
