2add1688aa449dea552647143114bc36bb1caac1
arnoldrx <tagnearnold2080@gmail.com>
2025-04-28 18:34:32 +0100

    fix

--- ppta_common/models/financial_line.py
+++ ppta_common/models/financial_line.py
@@ -1,4 +1,4 @@
-from mongoengine import StringField,IntField, ReferenceField, FloatField, DictField
+from mongoengine import StringField,IntField, ReferenceField, FloatField
 from .base_document import BaseDocument
 from ppta_common.models.session import Session
 from ppta_common.models.coaching import Coaching
@@ -13,13 +13,13 @@ class FinancialLine(BaseDocument):
     transaction_id = StringField(required=False)
     client = StringField(required=False)
     amount = FloatField(required=False)
-    start_date  = IntField(required=True)
-    end_date  = IntField(required=True)
+    start_date = IntField(required=True)
+    end_date = IntField(required=True)
     coaching = ReferenceField(Coaching, required=True)
     coach = ReferenceField(Coach, required=True)
     type = StringField(choices=[e.value for e in EnumFinancialLineType], required=True)
     paid_at = IntField(default=False)
     status = StringField(choices=[e.value for e in EnumFinancialLineStatus], required=True)
     cancelled_at = IntField(required=False)
+    invoice_link = StringField(required=False)
     destination = StringField(choices=[e.value for e in EnumDestinationStatus], required=True)
-    metadata = DictField(required=False)
