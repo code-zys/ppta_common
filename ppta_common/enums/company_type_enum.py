b085b7f5a945d3f6165ea156beaefb61e96607f1
arnoldrx <tagnearnold2080@gmail.com>
2025-05-15 15:11:24 +0100

    new changes

--- ppta_common/enums/company_type_enum.py
+++ ppta_common/enums/company_type_enum.py
@@ -2,7 +2,11 @@ from enum import Enum
 
 
 class CompanyType(str, Enum):
-    CERTIFIED_ACCOUNTANT = 'CERTIFIED_ACCOUNTANT'
-    ESN = 'ESN'
-    FREELANCE = 'FREELANCE'
-    FINAL_CLIENT = 'FINAL_CLIENT'
+    CERTIFIED_ACCOUNTANT = 'CERTIFIED_ACCOUNTANT' #TODO: To be removed
+    ESN = 'ESN' #TODO: To be removed
+    FREELANCE = 'FREELANCE' #TODO: To be removed
+    
+    FINAL_CLIENT = 'FINAL_CLIENT' # Replaces FREELANCE
+    ESN_SOURCING_RH = 'ESN_SOURCING_RH' # Replaces ESN
+    CONSULTANT = 'CONSULTANT'
+    COACH = 'COACH'
