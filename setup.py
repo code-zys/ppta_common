c2604ce12b84f740ea4e0c5e244ec5b14d512715
arnoldrx <tagnearnold2080@gmail.com>
2025-05-15 17:51:00 +0100

    update the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.6.22',
+    version='0.6.23',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
