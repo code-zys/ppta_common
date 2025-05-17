0948d50a3ca24563657bfd057c8ea399c6caf1dd
cedric tanda <tandacedric@gmail.com>
2025-05-17 15:39:51 +0100

    Update payment object

--- ppta_common/models/payment.py
+++ ppta_common/models/payment.py
@@ -7,18 +7,26 @@ from .payment_detail import PaymentDetails
 from .base_document import BaseDocument
 from .billing_details import BillingDetails
 from .coaching import Coaching
+from .company import Company
 
 class Payment(BaseDocument):
     amount=IntField(required=True)
     currency= StringField(choices=[e.value for e in EnumCurencyType], required=True)
     received_date = IntField( required=True)
     is_successful = BooleanField( required=False)
-    payment_method_details = EmbeddedDocumentField(PaymentDetails, required=False, description="Details du payment", default=None)
-    customer = EmbeddedDocumentField(Customer, required=False, description="The card of the payment", default=None)
-    card = EmbeddedDocumentField(Card, description="The card of the payment", required=False)
-    type = StringField(choices=[e.value for e in EnumCardType], required=False)
+    payment_method_details = EmbeddedDocumentField(PaymentDetails, required=False, description="Details du payment", default=None)#TODO: To be removed
+    customer = EmbeddedDocumentField(Customer, required=False, description="The card of the payment", default=None)#TODO: To be removed
+    card = EmbeddedDocumentField(Card, description="The card of the payment", required=False)#TODO: To be removed
+    type = StringField(choices=[e.value for e in EnumCardType], required=False)#TODO: To be removed
     coach_percentage = FloatField(required=False) #TODO: To be removed
     invoice_link = StringField(required=False)
+    payment_method_id = StringField(required=False)
+    company = ReferenceField(Company, required=True)
+    stripe_customer_id = StringField(required=False)
     transaction_id= StringField(required=False)
     billing_details = EmbeddedDocumentField(BillingDetails, required=True)
     coaching_id = StringField(required=False, description="The coaching of the payment" )
+    
+    meta = {
+        "collection": "coaching_payments",
+    }
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppta_common',
-    version='0.6.35',
+    version='0.6.36',
     packages=find_packages(),
     install_requires=[
         'mongoengine',
